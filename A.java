8a7a7b9cff94e5ce7d1f581f472db80116470f3d
Mariana Sales <mariana.sales2@estudante.ufla.br>
2023-10-02 17:20:56 -0300

    Criação da classe C

--- A.java
+++ A.java
@@ -25,4 +25,8 @@ public class A {
     public void MA2() {
         System.out.println("Método MA2 da classe A");
     }
+
+    public void MA3() {
+        System.out.println("Alteração a classe A partir do clone");
+    }
 }
---
+++ C.java
@@ -0,0 +1,12 @@
+public class C {
+    private String C1;
+    private int C2;
+
+    public void MC1() {
+        System.out.println("Método MC1 da classe C");
+    }
+
+    public void MC2() {
+        System.out.println("Método MC2 da classe C");
+    }
+}
